d8f4425d92bcd3a8801f73cdb82fd8f030304c77
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-04-26 16:11:57 +0200

    open database file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::path::Path;
+use std::io;
+use positioned_io::{RandomAccessFile, ReadBytesAtExt as _ };
+
+#[derive(Debug)]
+struct Database {
+    raf: RandomAccessFile,
+    info: DatabaseInfo,
+}
+
+#[derive(Debug)]
+struct DatabaseInfo {
+    db_type: u8,
+}
+
+impl Database {
+    fn open<P: AsRef<Path>>(path: P) -> io::Result<Database> {
+        let raf = RandomAccessFile::open(path)?;
+
+        Ok(Database {
+            info: DatabaseInfo {
+                db_type: raf.read_u8_at(0)?,
+            },
+            raf
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let db = Database::open("IP2PROXY-IP-PROXYTYPE-COUNTRY.BIN").unwrap();
+    dbg!(db);
 }
