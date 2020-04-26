09b96f5e86087f6558df605d59b1dd9b094b222f
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-04-26 21:56:09 +0200

    test an ip that is really in the file

--- tests/test.rs
+++ tests/test.rs
@@ -4,7 +4,12 @@ use ip2proxy::Database;
 #[test]
 fn test_ipv4() -> io::Result<()> {
     let database = Database::open("data/IP2PROXY-LITE-PX8.BIN")?;
-    let ip = "8.8.8.8".parse().unwrap();
+    dbg!(&database.info);
+
+    //let ip = "8.8.8.8".parse().unwrap();
+    //let ip = "127.0.0.1".parse().unwrap();
+
+    let ip = "1.0.104.238".parse().unwrap();
     let row = dbg!(database.query_ipv4(ip)?.unwrap());
     Ok(())
 }
