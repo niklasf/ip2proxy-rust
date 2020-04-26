3229aaf8dc60ca62a47f90bcb5ffda24dedc5497
Niklas Fiekas <niklas.fiekas@backscattering.de>
2020-04-26 16:23:54 +0200

    read complete header

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use std::io;
-use positioned_io::{RandomAccessFile, ReadBytesAtExt as _ };
+use positioned_io::{RandomAccessFile, ReadBytesAtExt as _};
+use byteorder::LE;
 
 #[derive(Debug)]
 struct Database {
@@ -10,7 +11,17 @@ struct Database {
 
 #[derive(Debug)]
 struct DatabaseInfo {
-    db_type: u8,
+    px: u8,
+    columns: u8,
+    year: u8,
+    month: u8,
+    day: u8,
+    rows: u32,
+    base_addr: u32,
+    rows_ipv6: u32,
+    base_addr_ipv6: u32,
+    index_base_addr: u32,
+    index_base_addr_ipv6: u32,
 }
 
 impl Database {
@@ -19,7 +30,17 @@ impl Database {
 
         Ok(Database {
             info: DatabaseInfo {
-                db_type: raf.read_u8_at(0)?,
+                px: raf.read_u8_at(0)?,
+                columns: raf.read_u8_at(1)?,
+                year: raf.read_u8_at(2)?,
+                month: raf.read_u8_at(3)?,
+                day: raf.read_u8_at(4)?,
+                rows: raf.read_u32_at::<LE>(5)?,
+                base_addr: raf.read_u32_at::<LE>(9)?,
+                rows_ipv6: raf.read_u32_at::<LE>(13)?,
+                base_addr_ipv6: raf.read_u32_at::<LE>(17)?,
+                index_base_addr: raf.read_u32_at::<LE>(21)?,
+                index_base_addr_ipv6: raf.read_u32_at::<LE>(25)?,
             },
             raf
         })
